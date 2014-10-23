1cd5e47dcbb4d2fb7c3581fd8a42719805f0070e
Satbir Saini <satbir.kira@gmail.com>
2014-10-23 15:12:23 -0400

    added getters and setters test

--- model-tests.py
+++ model-tests.py
@@ -1,14 +1,44 @@
 import unittest
-from mvc import Model
-from mvc import State, Format, Quality, titleFormat, videoState
+from model import Model
+from constants import State, Format, Quality, titleFormat, videoState
+
  
-class TestUM(unittest.TestCase):
+class TestModel(unittest.TestCase):
  
     def setUp(self):
         pass
  
     def test_default_HQ_FLV(self):
-        self.assertEqual(Format.FLV, Format.FLV)
+        model = Model()
+        self.assertEqual(model.program_status, State.NO_OPEN_FILE)
+        
+    def test_default_use_bookmark_title(self):
+        model = Model()
+        self.assertEqual(model.output_title_format, titleFormat.USE_BOOKMARK_TITLE)
+
+    def test_default_status(self):
+        model = Model()
+        self.assertEqual(model.program_status, State.NO_OPEN_FILE)
+
+    def test_gets_and_sets(self):
+        model = Model()
+        
+        model.setFilePath("C:\bookmark.html")
+        model.setOutputPath("C:\downloads")
+        model.setOutputFormat(Format.WAV)
+        model.setOutputTitleFormat(titleFormat.USE_YOUTUBE_TITLE)
+        model.setOutputQuality(Quality.HIGH)
+        model.setStatus(State.OPENING_FILE)
+
+        self.assertEqual(model.getFilePath, "C:\bookmark.html")
+        self.assertEqual(model.getOutputPath, "C:\downloads")
+        self.assertEqual(model.getOutputFormat, Format.WAV)
+        self.assertEqual(model.getOutputTitleFormat, titleFormat.USE_YOUTUBE_TITLE)
+        self.assertEqual(model.getOutputQuality, Quality.HIGH)
+        self.assertEqual(model.getStatus, State.OPENING_FILE)
+
+
+
 
  
 if __name__ == '__main__':
